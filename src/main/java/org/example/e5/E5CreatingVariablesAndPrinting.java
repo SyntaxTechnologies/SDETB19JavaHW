5d2b5b11e94896d6983f91361f76a987e0662be7
manal <manalmatta706@gmail.com>
2024-02-26 09:29:09 -0500

    first 4 hw

--- src/main/java/org/example/e5/E5CreatingVariablesAndPrinting.java
+++ src/main/java/org/example/e5/E5CreatingVariablesAndPrinting.java
@@ -2,13 +2,13 @@ package org.example.e5;
 
 public class E5CreatingVariablesAndPrinting {
     public static void main(String[] args) {
-        String name="chen";
-        int age=50;
-        int iq=age;
+        String name ="Chen";
+        int age = 50;
+        int iq = age;
+
         System.out.println(name);
         System.out.print(age);
-        System.out.println(iq);
-
+        System.out.print(iq);
 
 
     }
--- src/main/java/org/example/e6/E6StringConcatenation.java
+++ src/main/java/org/example/e6/E6StringConcatenation.java
@@ -2,9 +2,10 @@ package org.example.e6;
 
 public class E6StringConcatenation {
     public static void main(String[] args) {
-        String firstName="John";
-        String lastName="Smith";
-        System.out.println(firstName+" "+lastName);
+
+        String var1 = "John";
+        String var2 = "Smith";
+        System.out.print(var1+" "+var2);
 
     }
 }
